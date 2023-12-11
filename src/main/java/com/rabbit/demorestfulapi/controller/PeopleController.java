5d12e79231d197d4afaa02f93a8b675db7677882
kdukoelho <ce.figueiredoc@gmail.com>
2023-12-11 16:16:07 -0300

    add: GetPeopleById endpoint

--- src/main/java/com/rabbit/demorestfulapi/controller/PeopleController.java
+++ src/main/java/com/rabbit/demorestfulapi/controller/PeopleController.java
@@ -8,29 +8,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.rabbit.demorestfulapi.dto.PeopleResponseDTO;
 import com.rabbit.demorestfulapi.repository.PeopleRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/peoples")
 public class PeopleController {
-	
+
 	@Autowired
 	private PeopleRepository repository;
+
+	@CrossOrigin(origins = "http://localhost:8080", allowedHeaders =  "*")
 	@PostMapping
 	public void postPeople(@RequestBody PeopleRequestDTO peopleRequestDTO){
 		People people = new People(peopleRequestDTO);
 		repository.save(people);
 	}
 
+	@CrossOrigin(origins = "http://localhost:8080", allowedHeaders =  "*")
 	@GetMapping
-	public List<PeopleResponseDTO> getPeoples() {
-
+	public List<PeopleResponseDTO> getPeoplesList() {
 		List<PeopleResponseDTO> productList = repository.findAll().stream().map(PeopleResponseDTO::new).toList();
-		
 		return productList;
+	}
+
+	@CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
+	@GetMapping("/{id}")
+	public PeopleResponseDTO getPeopleById(@PathVariable Long id){
+		People people = repository.findById(id).get();
+		return new PeopleResponseDTO(people);
 	}
 }
