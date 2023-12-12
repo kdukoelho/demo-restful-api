33807d436d34de7c4bfe1e8f13a46690b96dcd4f
kdukoelho <ce.figueiredoc@gmail.com>
2023-12-12 20:12:16 -0300

    feat: automatic MySQL population

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,16 +1,14 @@
-INSERT INTO tb_departments(id, dep_name) VALUES
-(1, "Development"),
-(2, "Marketing"),
-(3, "Humans Resources");
+INSERT INTO tb_departments(id, dep_name) VALUES (1, "Development");
+INSERT INTO tb_departments(id, dep_name) VALUES (2, "Marketing");
+INSERT INTO tb_departments(id, dep_name) VALUES (3, "Humans Resources");
 
-INSERT INTO tb_peoples(id, people_name, email, department_id) VALUES
-(1, "Fernanda Agatha Lima", "fernanda_lima@bds.com.br", 1),
-(2, "Rafaela Yasmin da Rocha", "rafaela_darocha@fosjc.unesp.br", 3),
-(3, "Guilherme Bento Iago da Silva", "guilhermebentodasilva@teravida.com.br", 1),
-(4, "Bárbara Brenda Drumond", "barbara-drumond96@citi.com", 1),
-(5, "Kaique Manoel Souza", "kaique_manoel_souza@fclar.net.br", 1),
-(6, "Tiago Mário Freitas", "tiago_mario_freitas@arosacampinas.com.br", 1),
-(7, "Antonella Aurora Aragão", "antonellaauroraaragao@gmx.com.br", 2),
-(8, "Theo Luiz Pietro Vieira", "theo_vieira@gamil.com", 2),
-(9, "Gabriela Regina Baptista", "gabrielareginabaptista@vipsaude.com.br", 1),
-(10, "Thales Daniel Juan Nunes", "thales.daniel.nunes@nhrtaxiaereo.com", 3);
+INSERT INTO tb_employees(employee_name, email, department_id) VALUES ("Fernanda Agatha Lima", "fernanda_lima@bds.com.br", 1);
+INSERT INTO tb_employees(employee_name, email, department_id) VALUES ("Rafaela Yasmin da Rocha", "rafaela_darocha@fosjc.unesp.br", 3);
+INSERT INTO tb_employees(employee_name, email, department_id) VALUES ("Guilherme Bento Iago da Silva", "guilhermebentodasilva@teravida.com.br", 1);
+INSERT INTO tb_employees(employee_name, email, department_id) VALUES ("Bárbara Brenda Drumond", "barbara-drumond96@citi.com", 1);
+INSERT INTO tb_employees(employee_name, email, department_id) VALUES ("Kaique Manoel Souza", "kaique_manoel_souza@fclar.net.br", 1);
+INSERT INTO tb_employees(employee_name, email, department_id) VALUES ("Tiago Mário Freitas", "tiago_mario_freitas@arosacampinas.com.br", 1);
+INSERT INTO tb_employees(employee_name, email, department_id) VALUES ("Antonella Aurora Aragão", "antonellaauroraaragao@gmx.com.br", 2);
+INSERT INTO tb_employees(employee_name, email, department_id) VALUES ("Theo Luiz Pietro Vieira", "theo_vieira@gamil.com", 2);
+INSERT INTO tb_employees(employee_name, email, department_id) VALUES ("Gabriela Regina Baptista", "gabrielareginabaptista@vipsaude.com.br", 1);
+INSERT INTO tb_employees(employee_name, email, department_id) VALUES ("Thales Daniel Juan Nunes", "thales.daniel.nunes@nhrtaxiaereo.com", 3);
