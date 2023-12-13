47ee3c4b829a4898779853de882aff780362ec03
kdukoelho <ce.figueiredoc@gmail.com>
2023-12-12 21:10:03 -0300

    fix: DepartmentController GetMapping value

--- src/main/java/com/rabbit/demorestfulapi/controller/DepartmentController.java
+++ src/main/java/com/rabbit/demorestfulapi/controller/DepartmentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(values = "/departments")
+@RequestMapping(value = "/departments")
 public class DepartmentController {
     @Autowired
     DepartmentRepository repository;
