03414632b6a142abc08d0673e5d24df845322d76
kdukoelho <ce.figueiredoc@gmail.com>
2024-01-25 10:48:34 -0300

    chore: refactor project name

--- src/main/java/com/rabbit/demorestfulapi/DepartmentsManagementApi.java
+++ src/main/java/com/rabbit/demorestfulapi/DepartmentsManagementApi.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoRestfulApiApplication {
+public class DepartmentsManagementApi {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoRestfulApiApplication.class, args);
+		SpringApplication.run(DepartmentsManagementApi.class, args);
 	}
 
 }
